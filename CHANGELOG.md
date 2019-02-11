a9d6eaecc331d1c0f0fbb8305cef1ff7b9d044ab
skyjedi <skyjedi@gmail.com>
2019-02-11 12:48:52 -0600

    # 1.0.1     Code Spilting!     made changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# 1.0.1
+    Code Spilting! 
+    made changelog
+# 1.0.0 
+    init ( very bad code that i am ashamed of)
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,34 @@
-export {default as App} from './App';
-export {default as Config} from './Config';
-export {default as Live_Config} from './Live_Config';
-export {default as Home} from './Home';
-export {default as Mobile} from './Mobile';
-export {default as Panel} from './Panel';
-export {default as Video_Component} from './Video_Component';
-export {default as Video_Overlay} from './Video_Overlay';
+import React, {Component} from "react";
+
+const asyncComponent = (importComponent) => {
+	class AsyncComponent extends Component {
+		constructor(props) {
+			super(props);
+			this.state = {component: null};
+		}
+
+		async componentDidMount() {
+			const {default: component} = await importComponent();
+			this.setState({component: component});
+		}
+
+		render() {
+			const C = this.state.component;
+			return C ? <C {...this.props} /> : null;
+		}
+	}
+
+	return AsyncComponent;
+};
+
+export const App = asyncComponent(() => import('./App'));
+export const Config = asyncComponent(() => import('./Config'));
+export const Home = asyncComponent(() => import('./Home'));
+export const Live_Config = asyncComponent(() => import('./Live_Config'));
+export const Mobile = asyncComponent(() => import('./Mobile'));
+export const Panel = asyncComponent(() => import('./Panel'));
+export const Video_Component = asyncComponent(() => import('./Video_Component'));
+export const Video_Overlay = asyncComponent(() => import('./Video_Overlay'));
+
+
+
